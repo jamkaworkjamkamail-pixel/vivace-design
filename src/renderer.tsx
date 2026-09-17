99c4a681de958df8fd4a81a966a8d577f7715645
Vivace Dev <dev@vivace.mn>
2026-09-17 06:30:05 +0000

    Add Meta Pixel (ID 28447802321521588) to global head

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -94,6 +94,27 @@ export const renderer = jsxRenderer(({ children, title, description }) => {
   }
 })();
         `}} />
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{__html: `
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '28447802321521588');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=28447802321521588&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+        `}} />
+        {/* End Meta Pixel Code */}
       </head>
       <body>
         <div id="page-transition"></div>
